6cbe69eeea83421c51600623e3542a1835c2e386
Benjamin23115 <benjaminfarias313@gmail.com>
2023-04-07 12:03:28 -0700

    Added mergesort function + helper function.

--- sequentialsorts.h
+++ sequentialsorts.h
@@ -8,12 +8,12 @@
  */
 void selectionSort(int *array, int size)
 {
-    for(int j = 0; j < size; j++)
+    for (int j = 0; j < size; j++)
     {
         int smallest = j;
-        for(int i = j+1; i < size; i++)
+        for (int i = j + 1; i < size; i++)
         {
-            if(array[i] < array[smallest])
+            if (array[i] < array[smallest])
             {
                 smallest = i;
             }
@@ -32,11 +32,11 @@ void insertionSort(int *array, int size)
     {
         /* code */
         int current = i;
-        while(current > 0 && array[current] < array[current-1])
+        while (current > 0 && array[current] < array[current - 1])
         {
             int temp = array[current];
-            array[current] = array[current-1];
-            array[current-1] = temp;
+            array[current] = array[current - 1];
+            array[current - 1] = temp;
             current--;
         }
     }
@@ -50,18 +50,69 @@ void bubbleSort(int *array, int size)
     {
         swapped = false;
         // loop over each element of the array
-        for(int i = 0; i < size-1; i++)
+        for (int i = 0; i < size - 1; i++)
         {
             // Compare with the element immediately after
-            if(array[i] > array[i+1])
+            if (array[i] > array[i + 1])
             {
                 // if the item is greater than it's neighbor, swap
                 int temp = array[i];
-                array[i] = array[i+1];
-                array[i+1] = temp;
+                array[i] = array[i + 1];
+                array[i + 1] = temp;
                 swapped = true;
             }
         }
 
-    }while(swapped);
+    } while (swapped);
+}
+void merge(int *array, int left, int middle, int right)
+{
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+    int L[n1];
+    int R[n2];
+    for (int i = 0; i < n1; i++)
+        L[i] = array[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = array[middle + 1 + j];
+    int i, j, k;
+    i = 0;
+    j = 0;
+    k = left;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            array[k] = L[i];
+            i++;
+        }
+        else
+        {
+            array[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        array[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        array[k] = R[j];
+        j++;
+        k++;
+    }
+}
+void mergeSort(int *array, int left, int right)
+{
+    if (left < right)
+    {
+        int middle = (left + (right - left) / 2);
+        mergeSort(array, left, middle);
+        mergeSort(array, middle + 1, right);
+        merge(array, left, middle, right);
+    }
 }
--- sorting.cpp
+++ sorting.cpp
@@ -7,16 +7,16 @@ const int ARRAY_SIZE = 14;
 
 int main(int argc, char const *argv[])
 {
-    int array[] = {5, 37, 15, 24, 2, 65, 7, 9, -1, 15,-7, 88, 77, 0};
+    int array[] = {5, 37, 15, 24, 2, 65, 7, 9, -1, 15, -7, 88, 77, 0};
 
     cout << "Selection Sort" << endl;
-    int* copy = generateIntArrayCopy(array, ARRAY_SIZE);
-    
+    int *copy = generateIntArrayCopy(array, ARRAY_SIZE);
+
     printIntArray(copy, ARRAY_SIZE);
     selectionSort(copy, ARRAY_SIZE);
     printIntArray(copy, ARRAY_SIZE);
 
-    delete [] copy;
+    delete[] copy;
 
     cout << "========================================" << endl;
     cout << "Insertion Sort" << endl;
@@ -25,7 +25,7 @@ int main(int argc, char const *argv[])
     insertionSort(copy, ARRAY_SIZE);
     printIntArray(copy, ARRAY_SIZE);
 
-    delete [] copy;
+    delete[] copy;
 
     cout << "========================================" << endl;
     cout << "Bubble Sort" << endl;
@@ -34,8 +34,16 @@ int main(int argc, char const *argv[])
     bubbleSort(copy, ARRAY_SIZE);
     printIntArray(copy, ARRAY_SIZE);
 
-    delete [] copy;
+    delete[] copy;
 
+    cout << "========================================" << endl;
+    cout << "Merge Sort" << endl;
+    copy = generateIntArrayCopy(array, ARRAY_SIZE);
+    printIntArray(copy, ARRAY_SIZE);
+    mergeSort(copy, 0, ARRAY_SIZE - 1);
+    printIntArray(copy, ARRAY_SIZE);
+
+    delete[] copy;
 
     return 0;
 }
